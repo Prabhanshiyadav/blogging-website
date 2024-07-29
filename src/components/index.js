fe745158ba7a5c1e52f0de62836f78ec23a04f17
Prabhanshiyadav <prabhanshi04@gmail.com>
2024-07-29 21:37:48 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+
+export {default as GuestRoute} from './GuestRoute';
+export {default as AuthRoute} from './GuestRoute';
+export {default as Navbar} from './Navbar';
+export {default as FormErrors} from './FormErrors';
+export {default as TagsInput} from './Tagsinput';
+export {default as ArticleList} from './Articlelist';
+
+export {default as ArticlePreview} from './Articlepreview';
+
+export {default as ArticleMeta} from './ArticleMeta';
